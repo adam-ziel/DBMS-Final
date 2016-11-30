bbaba4d0861bc1a5ec0fafb081e304d21327b17d
julinpatel95 <julinpatel95@gmail.com>
2016-11-29 20:37:11 -0500

    Update StudentDML.sql

--- StudentDML.sql
+++ StudentDML.sql
@@ -2,5 +2,15 @@ INSERT INTO Student VALUES (1, “George”, “Lucas”, “CIVE”);
 INSERT INTO Student VALUES (2, “Bobby”, “Laughlin”, “CS”);
 INSERT INTO Student VALUES (3, “Julie”, “Medeiros”, “CN”);
 INSERT INTO Student VALUES (4, “Dave”, “Lee”, “ME”);
-
-
+INSERT INTO Student VALUES (5, “Anthony”, “A”, “CS”);
+INSERT INTO Student VALUES (6, “Bachar”, “Al Hardy”, “EM”);
+INSERT INTO Student VALUES (7, “Gregory”, “Butterworth”, “EE”);
+INSERT INTO Student VALUES (8, “Daniel”, “Calderon”, “CS”);
+INSERT INTO Student VALUES (9, “Stephen”, “Campion”, “CS”);
+INSERT INTO Student VALUES (10, “Nick”, “Chase”, “ME”);
+INSERT INTO Student VALUES (11, “Shubh”, “Chokshi”, “EE”);
+INSERT INTO Student VALUES (12, “Bethy”, “Diakabana”, “CS”);
+INSERT INTO Student VALUES (13, “Bret”, “Glendye”, “ME”);
+INSERT INTO Student VALUES (14, “Lyn”, “Carswell”, “EE”);
+INSERT INTO Student VALUES (15, “Jojo”, “J”, “EE”);
+INSERT INTO Student VALUES (16, “Bret”, “Glendye”, “ME”);
