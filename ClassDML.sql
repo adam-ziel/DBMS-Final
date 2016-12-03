26e4038697d2c4eeea0fd96e578d54af665b2518
DManuel010 <manueld@wit.edu>
2016-12-03 17:37:28 -0500

    Changed all curly quotations to straight quotations

--- ClassDML.sql
+++ ClassDML.sql
@@ -1,9 +1,7 @@
-INSERT INTO Class VALUES (“COMP-1000”, “Computer Science I” , “Fall”, 2016, 2, 4);
-INSERT INTO Class VALUES (“COMP-1100”, “Intro To Networks”, “Fall”, 2016, 2, 2);
-INSERT INTO Class VALUES (“COMP-2500”, “Security Principles”, “Fall”, 2016, 2, 3);
-INSERT INTO Class VALUES (“COMP-2000”, “Data Structures”, “Fall”, 2016, 2, 3);
-INSERT INTO Class VALUES (“COMP-3071”, “Intro To Programming Languages”, “Fall”, 2016, 2, 3);
-INSERT INTO Class VALUES (“CIVE-2000”, “Statics and Mechanics of Materials 1”, “Fall”, 2016, 5, 7);
-INSERT INTO Class VALUES (“CIVE-3800”, “EPIC Projects in Civil Engineering 1”, “Fall”, 2016, 5, 6);
-
-
+INSERT INTO Class VALUES ("COMP-1000", "Computer Science I" , "Fall", 2016, 2, 4);
+INSERT INTO Class VALUES ("COMP-1100", "Intro To Networks", "Fall", 2016, 2, 2);
+INSERT INTO Class VALUES ("COMP-2500", "Security Principles", "Fall", 2016, 2, 3);
+INSERT INTO Class VALUES ("COMP-2000", "Data Structures", "Fall", 2016, 2, 3);
+INSERT INTO Class VALUES ("COMP-3071", "Intro To Programming Languages", "Fall", 2016, 2, 3);
+INSERT INTO Class VALUES ("CIVE-2000", "Statics and Mechanics of Materials 1", "Fall", 2016, 5, 7);
+INSERT INTO Class VALUES ("CIVE-3800", "EPIC Projects in Civil Engineering 1", "Fall", 2016, 5, 6);
