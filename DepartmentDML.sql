ffab37db718d9ce885f2f3e20fa21ec116bdde5a
williamma23 <wma@williamma.xyz>
2016-12-04 16:31:22 -0500

    Updated Department , populated

--- DepartmentDML.sql
+++ DepartmentDML.sql
@@ -1,3 +1,10 @@
 INSERT INTO Department VALUES (3, "Civil Engineering and Technology", "BSCE");
 INSERT INTO Department VALUES (2, "Computer Science and Computer Networking", "BCOS");
 INSERT INTO Department VALUES (4, "Mechanical Engineering and Technology", "BSME");
+INSERT INTO Department VALUES (5, "Electrical Engineering and Technology", "BSEE");
+INSERT INTO Department VALUES (6, "Biomedical Engineering", "BMED");
+INSERT INTO Department VALUES (7, "Computer Science and Computer Networking", "BSCN");
+INSERT INTO Department VALUES (8, "Interdisciplinary Engineering", "BELM");
+
+
+
