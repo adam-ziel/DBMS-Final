baa135e6c73cfc791c3f829e6033f6aaa8fb7852
julinpatel95 <julinpatel95@gmail.com>
2016-12-04 14:37:16 -0500

    Update DepartmentDML.sql

--- DepartmentDML.sql
+++ DepartmentDML.sql
@@ -1,4 +1,4 @@
-INSERT INTO Department VALUES (3, "Civil Engineering and Technology", "CIVE");
-INSERT INTO Department VALUES (2, "Computer Science and Computer Networking", "CS");
-INSERT INTO Department VALUES (2, "Computer Science and Computer Networking", "CN");
-INSERT INTO Department VALUES (4, "Mechanical Engineering and Technology", "ME");
+INSERT INTO Department VALUES (3, "Civil Engineering and Technology", "BSCE");
+INSERT INTO Department VALUES (2, "Computer Science and Computer Networking", "BCOS");
+INSERT INTO Department VALUES (2, "Computer Science and Computer Networking", "BSCN");
+INSERT INTO Department VALUES (4, "Mechanical Engineering and Technology", "BSME");
