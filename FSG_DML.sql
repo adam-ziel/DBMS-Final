8491d59123039d84b79547a394d2ddfde4507d37
williamma23 <wma@williamma.xyz>
2016-12-04 15:14:13 -0500

    Updated FSG , fixed unique constraint

--- FSG_DML.sql
+++ FSG_DML.sql
@@ -1,2 +1,2 @@
 INSERT INTO FSG VALUES (1, "Wednesday", 1800, 2000, "Wentworth Hall", 214, 4, 3, "COMP-1000");
-INSERT INTO FSG VALUES (1, "Thursday", 1800, 2000, "Wentworth Hall", 214, 4, 3, "COMP-1000");
+INSERT INTO FSG VALUES (2, "Thursday", 1800, 2000, "Wentworth Hall", 214, 4, 3, "COMP-1000");
