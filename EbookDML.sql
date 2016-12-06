51b6e3d937bef2a241bf6d465c347d5ae1b65bc9
williamma23 <wma@williamma.xyz>
2016-12-05 22:05:21 -0500

    Populated with Networking ebooks

--- EbookDML.sql
+++ EbookDML.sql
@@ -3,5 +3,11 @@ INSERT INTO Ebook VALUES(9780133900699, "http://docs.oracle.com/javase/specs/jls
 INSERT INTO Ebook VALUES (9780134034089, "http://www.oracle.com/technetwork/java/javase/java-tutorial-downloads-2005894.html", "The Java Tutorials", "COMP-1000");
 INSERT INTO Ebook VALUES(9781441419200, "http://greenteapress.com/thinkapjava/thinkapjava.pdf", "Think Java (How to Think Like a Computer Scientist", "COMP-1000");
 INSERT INTO Ebook VALUES (9780131872486, "http://www.mindview.net/Books/TIJ", "Thinking in Java (3rd edition)", "COMP-1000");
+INSERT INTO Ebook VALUES (9780789749031, "https://www.amazon.com/Networking-Essentials-3rd-Jeffrey-Beasley/dp/0789749033/ref=sr_1_4?s=books&ie=UTF8&qid=1480993019&sr=1-4&keywords=networking", "Networking Essentials (3rd Edition)", "COMP-1100");
+INSERT INTO Ebook VALUES (9781511654944, "https://www.amazon.com/Introduction-Networking-How-Internet-Works/dp/1511654945/ref=sr_1_3?s=books&ie=UTF8&qid=1480993102&sr=1-3&keywords=computer+networking", "Introduction To Networking: How the Internet Works", "COMP-1100");
+INSERT INTO Ebook VALUES (9781119154723, "https://www.amazon.com/Networking-All-One-Dummies-Doug/dp/1119154723/ref=sr_1_4?s=books&ie=UTF8&qid=1480993186&sr=1-4&keywords=computer+networking", "Networking All-in-One For Dummies", "COMP-1100");
+INSERT INTO Ebook VALUES (9780132856201, "https://www.amazon.com/Computer-Networking-Top-Down-Approach-6th/dp/0132856204/ref=sr_1_5?s=books&ie=UTF8&qid=1480993186&sr=1-5&keywords=computer+networking", "Computer Networking: A Top-Down Approach (6th Edition)", "COMP-1100");
+INSERT INTO Ebook VALUES (9780071812245, "https://www.amazon.com/Networking-Beginners-Guide-Bruce-Hallberg/dp/0071812245/ref=sr_1_6?s=books&ie=UTF8&qid=1480993186&sr=1-6&keywords=computer+networking", "Networking: A Beginner's Guide, Sixth Edition", "COMP-1100");
+INSERT INTO Ebook VALUES (9781587201011, "https://www.amazon.com/Computer-Networking-First-Step-Wendell-Odom/dp/1587201011/ref=sr_1_14?s=books&ie=UTF8&qid=1480993450&sr=1-14&keywords=computer+networking", "Computer Networking First-Step", "COMP-1100");
 
                           
