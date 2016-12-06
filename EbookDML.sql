255b0c4ecb567781dbcb6e76e412ec5fccf71571
williamma23 <wma@williamma.xyz>
2016-12-05 21:52:55 -0500

    Populated more in Ebooks

--- EbookDML.sql
+++ EbookDML.sql
@@ -1,1 +1,7 @@
 INSERT INTO Ebook VALUES(28, "https://app.knovel.com/web/toc.v/cid:kpMHEGMH13/viewerType:toc/root_slug:machinerys-handbook-28th/url_slug:machinerys-handbook-28th/", "Machinery's Handbook 28th ed.", "MANF-2000");
+INSERT INTO Ebook VALUES(9780133900699, "http://docs.oracle.com/javase/specs/jls/se8/jls8.pdf", "The Java Lanaguage Specification Java SE 8 Edition", "COMP-1000");
+INSERT INTO Ebook VALUES (9780134034089, "http://www.oracle.com/technetwork/java/javase/java-tutorial-downloads-2005894.html", "The Java Tutorials", "COMP-1000");
+INSERT INTO Ebook VALUES(9781441419200, "http://greenteapress.com/thinkapjava/thinkapjava.pdf", "Think Java (How to Think Like a Computer Scientist", "COMP-1000");
+INSERT INTO Ebook VALUES (9780131872486, "http://www.mindview.net/Books/TIJ", "Thinking in Java (3rd edition)", "COMP-1000");
+
+                          
