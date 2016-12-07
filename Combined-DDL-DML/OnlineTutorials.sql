ec211e598ead1e22b6da3c33fb99873caa3e235e
Ziel Adam <ziela@wit.edu>
2016-12-07 17:56:23 -0500

    Online Tutorial had bad quotes in it

--- Combined-DDL-DML/OnlineTutorials.sql
+++ Combined-DDL-DML/OnlineTutorials.sql
@@ -1,4 +1,4 @@
-CREATE TABLE "OnlineTutorial" (
+CREATE TABLE `OnlineTutorial` (
 	`TutorialID`	INTEGER,
 	`URL`	TEXT UNIQUE,
 	`Title`	TEXT,
@@ -9,7 +9,7 @@ CREATE TABLE "OnlineTutorial" (
 );
 
 INSERT INTO OnlineTutorial VALUES(1, "https://www.lynda.com/Developer-Programming-Foundations-tutorials/Welcome/149042/177098-4.html", "Foundations of Programming: Data Structures", "Lynda", "COMP-2000");
-INSERT INTO OnlineTutorial VALUES(2,"https://www.khanacademy.org/computing/computer-science/algorithms/graph-representation/a/representing-graphs”, “Representing Graphs”, “Khan Academy”, “COMP-2000");
+INSERT INTO OnlineTutorial VALUES(2, "https://www.khanacademy.org/computing/computer-science/algorithms/graph-representation/a/representing-graphs", "Representing Graphs", "Khan Academy", "COMP-2000");
 INSERT INTO OnlineTutorial VALUES(3, "https://www.lynda.com/Developer-Programming-Foundations-tutorials/Understanding-binary-search-trees-BST/149042/177133-4.htm", "Understanding binary search trees (BST)", "Lynda", "COMP-2000");
 INSERT INTO OnlineTutorial VALUES(4, "https://www.lynda.com/HTML-tutorials/HTML-Structured-Data/363686-2.html?srchtrk=index%3a2%0alinktypeid%3a2%0aq%3adata+structures%0apage%3a1%0as%3arelevance%0asa%3atrue%0aproducttypeid%3a2", "HTML: Structured Data", "Lynda", "COMP-2000");
 INSERT INTO OnlineTutorial VALUES(5, "https://www.khanacademy.org/computing/computer-science/algorithms/intro-to-algorithms/v/what-are-algorithms","What is an algorithm?", "Khan Academy", "COMP-1000");
