18a7ff433f4936b1c81bc256077de01e0afcc99e
DManuel010 <manueld@wit.edu>
2016-12-07 18:51:21 -0500

    Removed and recreated new OfficeHour table.
    Added in all previous and new office hours

--- Combined-DDL-DML/OfficeHours.sql
+++ Combined-DDL-DML/OfficeHours.sql
@@ -18,4 +18,10 @@ INSERT INTO OfficeHours VALUES (4, "Friday", 5, 900, 1100, "9:00 AM", "11:00 AM"
 INSERT INTO OfficeHours VALUES (5, "Monday", 1, 900, 1100, "9:00 AM", "11:00 AM");
 INSERT INTO OfficeHours VALUES (7, "Monday", 1, 1100, 1200, "11:00 AM", "12:00 PM");
 INSERT INTO OfficeHours VALUES (7, "Wednesday", 3, 1100, 1200, "11:00 AM", "12:00 AM");
-INSERT INTO OfficeHours VALUES (7, "Friday", 5, 1100, 1200), "11:00 AM", "12:00 PM";
+INSERT INTO OfficeHours VALUES (7, "Friday", 5, 1100, 1200, "11:00 AM", "12:00 PM");
+INSERT INTO OfficeHours VALUES (8, "Monday", 1, 1100, 1200, "11:00 AM", "12:00 PM");
+INSERT INTO OfficeHours VALUES (8, "Tuesday", 2, 800, 950, "8:00 AM", "9:50 AM");
+INSERT INTO OfficeHours VALUES (8, "Wednesday", 3, 1100, 1200, "11:00 AM", "12:00 PM");
+INSERT INTO OfficeHours VALUES (8, "Friday", 5, 1100, 1200, "11:00 AM", "12:00 PM");
+INSERT INTO OfficeHours VALUES (9, "Tuesday", 2, 1000, 1200, "10:00 AM", "12:00 PM");
+INSERT INTO OfficeHours VALUES (9, "Thursday", 4, 1000, 1200, "10:00 AM", "12:00 PM");
