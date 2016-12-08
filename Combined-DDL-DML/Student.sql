a5a709caf19a4a8c4b16a09d3610ec9464cb1449
DManuel010 <manueld@wit.edu>
2016-12-08 16:57:05 -0500

    Reordered data in Student and StudentLeader files.
    Updated FSG table in DB to only have one student leader.
    
    Added an FSG for CIVE-2000.
    
    Added a BSCE student

--- Combined-DDL-DML/Student.sql
+++ Combined-DDL-DML/Student.sql
@@ -6,21 +6,35 @@ CREATE TABLE `Student` (
 	FOREIGN KEY(`MajorID`) REFERENCES `Major`(`MajorID`)
 );
 
+-- BSCE
 INSERT INTO Student VALUES (NULL, "George", "Lucas", "BSCE");
+
+-- BCOS
 INSERT INTO Student VALUES (NULL, "Bobby", "Laughlin", "BCOS");
-INSERT INTO Student VALUES (NULL, "Julie", "Medeiros", "BSCN");
-INSERT INTO Student VALUES (NULL, "David", "Lee", "BSME");
 INSERT INTO Student VALUES (NULL, "Anthony", "A", "BCOS");
-INSERT INTO Student VALUES (NULL, "Bachar", "Al Hardy", "BELM");
-INSERT INTO Student VALUES (NULL, "Gregory", "Butterworth", "BSEE");
 INSERT INTO Student VALUES (NULL, "Daniel", "Calderon", "BCOS");
 INSERT INTO Student VALUES (NULL, "Stephen", "Campion", "BCOS");
-INSERT INTO Student VALUES (NULL, "Nick", "Chase", "BSME");
-INSERT INTO Student VALUES (NULL, "Shubh", "Chokshi", "BSEE");
 INSERT INTO Student VALUES (NULL, "Bethy", "Diakabana", "BCOS");
-INSERT INTO Student VALUES (NULL, "Bret", "Glendye", "BSME");
-INSERT INTO Student VALUES (NULL, "Lyn", "Carswell", "BSEE");
-INSERT INTO Student VALUES (NULL, "Jojo", "J", "BSEE");
-INSERT INTO Student VALUES (NULL, "Abbey", "Jean", "BSAM");
 INSERT INTO Student VALUES (NULL, "Luis Jose", "Bechara Romero", "BCOS");
 INSERT INTO Student VALUES (NULL, "Jacob", "Jones", "BCOS");
+
+-- BSCN
+INSERT INTO Student VALUES (NULL, "Julie", "Medeiros", "BSCN");
+
+-- BSME
+INSERT INTO Student VALUES (NULL, "David", "Lee", "BSME");
+INSERT INTO Student VALUES (NULL, "Nick", "Chase", "BSME");
+INSERT INTO Student VALUES (NULL, "Bret", "Glendye", "BSME");
+INSERT INTO Student VALUES (NUll, "Andy", "Cortio", "BSCE");
+
+-- BELM
+INSERT INTO Student VALUES (NULL, "Bachar", "Al Hardy", "BELM");
+
+-- BSEE
+INSERT INTO Student VALUES (NULL, "Gregory", "Butterworth", "BSEE");
+INSERT INTO Student VALUES (NULL, "Shubh", "Chokshi", "BSEE");
+INSERT INTO Student VALUES (NULL, "Lyn", "Carswell", "BSEE");
+INSERT INTO Student VALUES (NULL, "Jojo", "J", "BSEE");
+
+-- BSAM
+INSERT INTO Student VALUES (NULL, "Abbey", "Jean", "BSAM");
--- Combined-DDL-DML/StudentLeader.sql
+++ Combined-DDL-DML/StudentLeader.sql
@@ -5,11 +5,19 @@ CREATE TABLE "StudentLeader" (
   FOREIGN KEY(`FSGID`) REFERENCES `FSG`(`FSGID`)
 );
 
+-- BSCE
+INSERT INTO StudentLeader VALUES (1, 7);
+INSERT INTO StudentLeader VALUES (5, 5);
+INSERT INTO StudentLeader VALUES (19, 8);
+INSERT INTO StudentLeader VALUES (19, 9);
+
+-- BCOS
+INSERT INTO StudentLeader VALUES (2, 3);
 INSERT INTO StudentLeader VALUES (3, 1);
 INSERT INTO StudentLeader VALUES (3, 2);
-INSERT INTO StudentLeader VALUES (2, 3);
-INSERT INTO StudentLeader VALUES (2, 7);
-INSERT INTO StudentLeader VALUES (2, 8);
+
+-- BSME
 INSERT INTO StudentLeader VALUES (4, 4);
-INSERT INTO StudentLeader VALUES (5, 5);
+
+-- BSEE
 INSERT INTO StudentLeader VALUES (6, 6);
