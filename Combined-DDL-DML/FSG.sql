858cdf91f868a7f5c81c74f29c7a9fd78affa902
DManuel010 <manueld@wit.edu>
2016-12-08 17:09:29 -0500

    Updated incorrect facultyID in FSG table and corrected DB.

--- Combined-DDL-DML/FSG.sql
+++ Combined-DDL-DML/FSG.sql
@@ -20,7 +20,7 @@ CREATE TABLE "FSG" (
 INSERT INTO FSG VALUES (1, "Wednesday", 3, 1800, 2000, "6:00 PM", "8:00 PM", "Wentworth Hall", 214, 44, 3, "COMP-1000");
 INSERT INTO FSG VALUES (2, "Thursday", 4, 1800, 2000, "6:00 PM", "8:00 PM", "Wentworth Hall", 214, 44, 3, "COMP-1000");
 INSERT INTO FSG VALUES (3, "Monday", 1, 1800, 2000, "6:00 PM", "8:00 PM", "Ira Allen", 203, 44, 2, "COMP-1100");
-INSERT INTO FSG VALUES (4, "Tuesday", 2, 1800, 2000, "6:00 PM", "8:00 PM", "Ira Allen", 203, 4, 4, "MECH-3100");
+INSERT INTO FSG VALUES (4, "Tuesday", 2, 1800, 2000, "6:00 PM", "8:00 PM", "Ira Allen", 203, 10, 4, "MECH-3100");
 INSERT INTO FSG VALUES (5, "Friday", 5, 1800, 2000, "6:00 PM", "8:00 PM", "Ira Allen", 203, 30, 1, "CIVE-3000");
 INSERT INTO FSG VALUES (6, "Monday", 1, 1800, 2000, "6:00 PM", "8:00 PM", "Ira Allen", 203, 8, 6, "ELEC-2299");
 INSERT INTO FSG VALUES (7, "Tuesday", 2, 1500, 1700, "3:00 PM", "5:00 PM", "Wentworth Hall", 214, 31, 1, "CIVE-2200");
