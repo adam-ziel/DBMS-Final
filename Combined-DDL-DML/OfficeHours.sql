f5f1cfe9a7e8b5a374b9471b3e30aac7cd9cfb6f
julinpatel95 <julinpatel95@gmail.com>
2016-12-08 15:08:39 -0500

    Update OfficeHours.sql

--- Combined-DDL-DML/OfficeHours.sql
+++ Combined-DDL-DML/OfficeHours.sql
@@ -9,19 +9,29 @@ CREATE TABLE `OfficeHours` (
 	FOREIGN KEY(`FacultyID`) REFERENCES Faculty(FacultyID)
 );
 
-INSERT INTO OfficeHours VALUES (2, "Tuesday", 2, 800, 950, "8:00 AM", "9:50 AM");
-INSERT INTO OfficeHours VALUES (2, "Thursday", 4, 800, 950, "8:00 AM", "9:50 AM");
-INSERT INTO OfficeHours VALUES (2, "Tuesday", 2, 1300, 1450, "1:00 PM", "2:50 PM");
-INSERT INTO OfficeHours VALUES (2, "Thursday", 4, 1300, 1450, "1:00 PM", "2:50 PM");
-INSERT INTO OfficeHours VALUES (4, "Wednesday", 3, 1100, 1200, "11:00 AM", "12:00 PM");
-INSERT INTO OfficeHours VALUES (4, "Friday", 5, 900, 1100, "9:00 AM", "11:00 AM");
-INSERT INTO OfficeHours VALUES (5, "Monday", 1, 900, 1100, "9:00 AM", "11:00 AM");
-INSERT INTO OfficeHours VALUES (7, "Monday", 1, 1100, 1200, "11:00 AM", "12:00 PM");
-INSERT INTO OfficeHours VALUES (7, "Wednesday", 3, 1100, 1200, "11:00 AM", "12:00 AM");
-INSERT INTO OfficeHours VALUES (7, "Friday", 5, 1100, 1200, "11:00 AM", "12:00 PM");
+
 INSERT INTO OfficeHours VALUES (8, "Monday", 1, 1100, 1200, "11:00 AM", "12:00 PM");
 INSERT INTO OfficeHours VALUES (8, "Tuesday", 2, 800, 950, "8:00 AM", "9:50 AM");
 INSERT INTO OfficeHours VALUES (8, "Wednesday", 3, 1100, 1200, "11:00 AM", "12:00 PM");
 INSERT INTO OfficeHours VALUES (8, "Friday", 5, 1100, 1200, "11:00 AM", "12:00 PM");
 INSERT INTO OfficeHours VALUES (9, "Tuesday", 2, 1000, 1200, "10:00 AM", "12:00 PM");
 INSERT INTO OfficeHours VALUES (9, "Thursday", 4, 1000, 1200, "10:00 AM", "12:00 PM");
+INSERT INTO OfficeHours VALUES (10, "Tuesday", 2, 800, 950, "8:00 AM", "9:50 AM");
+INSERT INTO OfficeHours VALUES (10, "Thursday", 4, 800, 950, "8:00 AM", "9:50 AM");
+INSERT INTO OfficeHours VALUES (10, "Tuesday", 2, 1300, 1450, "1:00 PM", "2:50 PM");
+INSERT INTO OfficeHours VALUES (10, "Thursday", 4, 1300, 1450, "1:00 PM", "2:50 PM");
+INSERT INTO OfficeHours VALUES (11, "Wednesday", 3, 1100, 1200, "11:00 AM", "12:00 PM");
+INSERT INTO OfficeHours VALUES (11, "Friday", 5, 900, 1100, "9:00 AM", "11:00 AM");
+INSERT INTO OfficeHours VALUES (11, "Monday", 1, 900, 1100, "9:00 AM", "11:00 AM");
+INSERT INTO OfficeHours VALUES (12, "Monday", 1, 1100, 1200, "11:00 AM", "12:00 PM");
+INSERT INTO OfficeHours VALUES (12, "Wednesday", 3, 1100, 1200, "11:00 AM", "12:00 AM");
+INSERT INTO OfficeHours VALUES (12, "Friday", 5, 1100, 1200, "11:00 AM", "12:00 PM");
+INSERT INTO OfficeHours VALUES (13, "Monday", 1, 1100, 1200, "11:00 AM", "12:00 PM");
+INSERT INTO OfficeHours VALUES (13, "Tuesday", 2, 800, 950, "8:00 AM", "9:50 AM");
+INSERT INTO OfficeHours VALUES (13, "Wednesday", 3, 1100, 1200, "11:00 AM", "12:00 PM");
+INSERT INTO OfficeHours VALUES (13, "Friday", 5, 1100, 1200, "11:00 AM", "12:00 PM");
+INSERT INTO OfficeHours VALUES (14, "Tuesday", 2, 1000, 1200, "10:00 AM", "12:00 PM");
+INSERT INTO OfficeHours VALUES (14, "Thursday", 4, 1000, 1200, "10:00 AM", "12:00 PM");
+INSERT INTO OfficeHours VALUES (15, "Monday", 1, 1100, 1200, "11:00 AM", "12:00 PM");
+INSERT INTO OfficeHours VALUES (15, "Tuesday", 2, 800, 950, "8:00 AM", "9:50 AM");
+INSERT INTO OfficeHours VALUES (15, "Wednesday", 3, 1100, 1200, "11:00 AM", "12:00 PM");
